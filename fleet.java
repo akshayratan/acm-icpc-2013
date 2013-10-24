2292880346877a7c20d08aaf032838fb98d728b9
Akshay Ratan <akshayratan@gmail.com>
2013-10-24 12:50:40 +0000

    Indented and Copyrighted the fleet program

--- fleet.java
+++ fleet.java
@@ -1,11 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Akshay
+ * @author Akshay Ratan with Team Algobusters in ACM ICPC Amritapuri Regional (C) 2013
  */
 
 import java.io.*;
@@ -18,23 +13,21 @@ public class fleet {
 
     public static void main(String[] args)throws IOException{
      
-       // BufferedReader obj=new BufferedReader(new InputStreamReader(System.in));
         Scanner sc=new Scanner(System.in);
         int test; //test cases
-        //test=Integer.parseInt(obj.readLine());
         test= sc.nextInt();
         int arr[]=new int[test];
         for(int i=0;i<test;i++)
         {
-            int n=sc.nextInt();
-            int m=sc.nextInt();
-            int t=sc.nextInt();
-            int j=0;
-            int ans=0;
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int t = sc.nextInt();
+            int j = 0;
+            int ans = 0;
             while(n>0)
             {
-                ans= ans+2*t;
-                n=n-m;
+                ans = ans+2*t;
+                n = n-m;
             }
             System.out.println(ans);
         }
